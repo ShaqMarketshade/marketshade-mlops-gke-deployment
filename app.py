9ccf2cb859b97a86504fdebccbc243e071376a39
ShaqMK <mburu.trades@gmail.com>
2026-01-08 16:05:25 +0300

    update2 app

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, request, jsonify # type: ignore
-import joblib # type: ignore
-import numpy as np # type: ignore
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
 import os
 
 app = Flask(__name__)
 
-# Load the trained model
+# Load the trained model and scaler
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "churn_prediction_model.pkl")
+SCALER_PATH = os.path.join(os.path.dirname(__file__), "scaler.pkl")
 model = joblib.load(MODEL_PATH)
+scaler = joblib.load(SCALER_PATH)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     features = np.array(data['features']).reshape(1, -1)
-    prediction = model.predict(features)
+    # Apply scaling
+    features_scaled = scaler.transform(features)
+    prediction = model.predict(features_scaled)
     return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
