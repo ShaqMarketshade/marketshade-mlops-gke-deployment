9894f814a8a133b12e9ae530f3008f1ec98b6d6b
ShaqMK <mburu.trades@gmail.com>
2026-01-08 14:15:45 +0300

    Updating app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the trained model
-model = joblib.load('churn_prediction_model.pkl')
+model = joblib.load('models/churn_prediction_model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
